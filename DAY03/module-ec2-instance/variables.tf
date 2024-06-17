abe42364d9a9e24563af49835150522d77ea436f
Shashwat Kamboj <57861101+kambojshashwat@users.noreply.github.com>
2024-06-17 22:33:39 +0530

    Create variables.tf

---
+++ DAY03/module-ec2-instance/variables.tf
@@ -0,0 +1,15 @@
+variable "ami_value" {
+    description = "Write Your AMI Value in modules section"
+}
+
+variable "instance_type_value" {
+    description = "Enter the Type of Instance you want to Launch"
+}
+
+variable "subnet_value" {
+    description = "Enter Your Subnet ID where you want to Launch EC2 Instance."
+}
+
+variable "subnet_value2" {
+    description = "Enter Your Subnet ID 2 where you want to Launch EC2 Instances"
+}
